a1529887f255c0fe3fb6ec2d2416a0d076d817bf
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-08 10:48:46 +0200

    ✨ show pokemon image accordint to game status

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,5 +1,8 @@
 <template>
-  <section v-if="loading" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section
+    v-if="loading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
     <h1 class="text-3xl">Please wait</h1>
     <h1 class="animate-pulse">Loading pokemons...</h1>
   </section>
@@ -7,9 +10,11 @@
   <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">Who is that pokemon?</h1>
 
-    {{ randomPokemon }}
     <!-- POKEMON IMGAE -->
-    <PokemonImage />
+    <PokemonImage
+      :pokemon-id="randomPokemon.id"
+      :show-pokemon="gameStatus !== GameStatus.Playing"
+    />
 
     <!-- POKEMON OPTIONS -->
     <PokemonOptions />
@@ -20,6 +25,7 @@
 import PokemonImage from '../components/PokemonImage.vue';
 import PokemonOptions from '../components/PokemonOptions.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
 
 const { gameStatus, randomPokemon, pokemonOptions, loading } = usePokemonGame();
 </script>
